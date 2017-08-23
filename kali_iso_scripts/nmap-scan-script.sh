f166844a37f2438d3320ffa1dbcfa1338451c765
flipflop1192 <20976552+flipflop1192@users.noreply.github.com>
2017-08-23 14:37:48 -0400

    Add files via upload

--- kali_iso_scripts/nmap-scan-script.sh
+++ kali_iso_scripts/nmap-scan-script.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+#Execute up to 4 parallel nmap scans
+#2 of the scans are subnet-wide: a ping scan and an ICS scan to look for potential ICS devices
+#The other 2 scans target a specific IP to get as much information from it as possible
+#This scan assumes the BACnet protocol is the primary target, but that can be modified as needed
+
+
+#create a folder to store the XML scan results
 read -p 'Enter the name of the folder for these to be stored: ' folder
 
 mkdir $folder
 cd $folder
 
+#ask for IP and determine if the target is just an address or also a subnet
 read -p 'Enter the IP address: ' ip
 read -p 'Scan the entire subnet as well (y/n)? ' response
 
+#if requested, carry out the 2 subnet scans
 if [ "$response" = "y" ] ; then
 	echo 'The CIDR mask /24 will be used for the subnet scans'
 
