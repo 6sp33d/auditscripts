b8c55462e42ac24ed47eb77c360e01d32b10cd7d
flipflop1192 <20976552+flipflop1192@users.noreply.github.com>
2017-08-23 14:02:07 -0400

    Update script-download.sh

--- misc/script-download.sh
+++ misc/script-download.sh
@@ -2,6 +2,6 @@
 
 cd /root/Documents/kali-live/live-build-config/config/includes.chroot/root/Desktop/
 
-exec svn checkout https://github.com/flipflop1192/auditscripts/trunk/scripts/
+exec svn checkout https://github.com/flipflop1192/auditscripts/trunk/kali_iso_scripts/
 
 exit $?
