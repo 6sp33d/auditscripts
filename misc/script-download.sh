2868c2e5029c21b83b6f8f119ec4894a075ab866
flipflop1192 <20976552+flipflop1192@users.noreply.github.com>
2017-08-23 14:38:38 -0400

    Add files via upload

--- misc/script-download.sh
+++ misc/script-download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Download audit scripts to load into the ISO as it's built
+
 cd /root/Documents/kali-live/live-build-config/config/includes.chroot/root/Desktop/
 
 exec svn checkout https://github.com/flipflop1192/auditscripts/trunk/kali_iso_scripts/
