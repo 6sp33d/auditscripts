cca581e9d414c2600d7af1cfec38857ad5f70516
flipflop1192 <20976552+flipflop1192@users.noreply.github.com>
2017-08-23 15:07:20 -0400

    Add files via upload

--- kali_iso_scripts/nmap-scan-script.sh
+++ kali_iso_scripts/nmap-scan-script.sh
@@ -21,17 +21,17 @@ if [ "$response" = "y" ] ; then
 	echo 'The CIDR mask /24 will be used for the subnet scans'
 
 	echo 'Initiating subnet ping scan...'
-	nmap -sn -oX $ip-subnet-ping-scan $ip\/24 &
+	(nmap -sn -v -oX $ip-subnet-ping-scan $ip\/24; echo '--------------------Subnet ping scan complete--------------------') &
 
 	echo 'Initiating subnet ICS scan...'
-	nmap -sT -sU -sV -p U:47808-47810,T:21-25,80,443,445,502-509,1911,3011,4011,4911,8080 -v -Pn --script banner -O -oX $ip-subnet-ICS-scan $ip\/24 &
+	(nmap -sT -sU -sV -p U:47808-47810,T:21-25,80,443,445,502-509,1911,3011,4011,4911,8080 -v -Pn --script banner -O -oX $ip-subnet-ICS-scan $ip\/24; echo '--------------------Subnet ICS scan complete--------------------') &
 
 fi
 
 echo 'Intiating BACnet interrogation...'
-nmap -sU -p 47808 -v -Pn --script bacnet-info -oX $ip-BACnet-interrogation $ip &
+(nmap -sU -p 47808 -v -Pn --script bacnet-info -oX $ip-BACnet-interrogation $ip; echo '--------------------BACnet interrogation complete--------------------') &
 
 echo 'Intiating ICS device port scan...'
-nmap -sT -sU -sV -p U:47808-47810,T:21-25,80,443,445,502-509,1911,3011,4011,4911,8080 -v -Pn --script banner -O -oX $ip-ICS-port-scan $ip
+(nmap -sT -sU -sV -p U:47808-47810,T:21-25,80,443,445,502-509,1911,3011,4011,4911,8080 -v -Pn --script banner -O -oX $ip-ICS-port-scan $ip; echo '--------------------ICS device port scan complete--------------------')
 
 exit $?
